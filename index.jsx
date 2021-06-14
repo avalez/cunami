fe0d554fab30cf7113f0d9fd45e31695164a0541
Andriy Zhdanov <azhdanov@gmail.com>
2021-06-14 12:21:39 +0200

    signer

--- index.jsx
+++ index.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { Signer } from '@waves/signer'
+import { ProviderSeed } from '@waves/provider-seed'
+
+const signer = new Signer({
+  NODE_URL: 'https://nodes-testnet.wavesnodes.com'
+})
+signer.setProvider(new ProviderSeed((new Date).toString()))
+
+var user
+signer.login().then(login => user = login.address)
 
 export default function HelloWorld() {
   function authFunc() {
-    WavesKeeper.auth({data: "Auth to the beatch"}).then(auth => alert(JSON.stringify(auth))).catch(console.log)
+    WavesKeeper.auth({data: "Auth to the beatch"}).then(auth => console.log(JSON.stringify(auth))).catch(console.log)
   }
   return (
     <div className="container">
       <p>
-        Welcome to the beatch!
+        Welcome to the beatch, {user}!
       </p>
       <input className="btn btn-primary" type="submit" value="Auth" onClick={authFunc}></input>
     </div>
